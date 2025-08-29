705c02cb1bedaeb7b238850449e1b1fd98266cf8
Wiktor Bandura <55361617+wiktor-bandura@users.noreply.github.com>
2025-08-29 10:49:55 +0200

    Update index.js

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,10 +2,10 @@ const burgerButton = document.querySelector('.navigation__mobile-button');
 const navigationWrapper = document.querySelector('.navigation__wrapper');
 
 burgerButton.addEventListener('click', () => {
-   navigationWrapper.style.display = 'block';
+   navigationWrapper.classList.add('slideInMenu');
 
    const closeButton = document.querySelector('.navigation__mobile-close-button');
    closeButton.addEventListener('click', () => {
-      navigationWrapper.style.display = 'none';
+      navigationWrapper.classList.remove('slideInMenu');
    });
 });
